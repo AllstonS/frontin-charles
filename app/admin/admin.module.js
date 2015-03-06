07ae824854801576f9c09d3108894300568daf09
allstons <jagschwartz@aol.com>
2015-03-06 14:02:41 -0500

    Allston's initial commit

---
+++ app/admin/admin.module.js
@@ -0,0 +1,24 @@
+(function () {
+  "use strict";
+
+  angular.module('frontin', [
+  'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+    .when('/admincompanies', {
+      templateUrl: '/admin/admin.companies.html',
+      controller: 'AdminController as adminCtrl'
+    })
+    .when('/addcompany', {
+      templateUrl: '/admin/admin.addcompany.html',
+      controller: 'AdminController as adminCtrl'
+    })
+    .when('/edit/:companyId', {
+      templateUrl: '/admin/admin.editcompany.html',
+      controller: 'AdminController as adminCtrl'
+    })
+
+  });
+
+})();
